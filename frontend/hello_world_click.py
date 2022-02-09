d6307839848ec42b96811b3377dc824dfe074127
AQuaintExpression <iliemarian.daniel@gmail.com>
2022-02-09 12:36:10 +0200

    initial setup

---
+++ frontend/hello_world_click.py
@@ -0,0 +1,17 @@
+import numbers
+import click
+
+
+@click.command()
+@click.argument('name')
+@click.option('--number', type=int, help="help message here")
+def main(name, number):
+    """
+    Help message
+    """
+    for i in range(number):
+        print(f'hello {name}')
+
+if __name__ == '__main__':
+    main()
+
